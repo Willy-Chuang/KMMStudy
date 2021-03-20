5685353218da15b2f45191fd9b926ead017106aa
Willy-Chuang <ws18950914@gmail.com>
2021-03-20 10:56:32 +0800

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/willychuang/kmmstudy/shared/Greeting.kt
@@ -0,0 +1,8 @@
+package com.willychuang.kmmstudy.shared
+
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/willychuang/kmmstudy/shared/Platform.kt
@@ -0,0 +1,5 @@
+package com.willychuang.kmmstudy.shared
+
+expect class Platform() {
+    val platform: String
+}
