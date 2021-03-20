ed2b1ee833ec956e1dab6d0435564ba4dd86dcc8
Willy-Chuang <ws18950914@gmail.com>
2021-03-20 12:03:31 +0800

    Create Logger

---
+++ shared/src/iosMain/kotlin/com/willychuang/kmmstudy/shared/Logger.kt
@@ -0,0 +1,19 @@
+package com.willychuang.kmmstudy.shared
+
+internal actual class Logger {
+    actual fun info(tag: String, message: String) {
+        print("Log info >>>>> $tag: $message")
+    }
+
+    actual fun debug(tag: String, message: String) {
+        print("Log Debug >>>>> $tag: $message")
+    }
+
+    actual fun warn(tag: String, message: String) {
+        print("Log Warn >>>>> $tag: $message")
+    }
+
+    actual fun error(tag: String, message: String) {
+        print("Log Error >>>>> $tag: $message")
+    }
+}
